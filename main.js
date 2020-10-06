c00734a4844d84cd6ef2e5c046c72f7cf5bb650e
Lin8787 <Lin8787>
2020-10-06 12:20:10 +0800

    Move to own directory.

--- main.js
+++ main.js
@@ -20,3 +20,51 @@ for(var i=0; i<themeButton.length; ++i) {
         document.body.setAttribute('class', e.target.id)
     }, false)
 }
+function type(){
+
+}
+
+function add(){
+	var input1 = document.getElementById("cal-input").value;
+	document.getElementById("demo").innerHTML = input1;
+}
+function click(){
+	//var elements = document.getElementsByClassName("date"); // get all elements
+	//for(var i = 0; i < elements.length; i++){
+	//	elements[i].style.backgroundColor = "black";
+	//}
+	document.getElementByClassName("date").style.color = "white";
+}
+var tds = document.querySelectorAll('.Cal-content-row table tr td');
+tds.forEach(function(td){
+  td.addEventListener('click', handleTDClick);
+});
+
+function handleTDClick(){
+	document.querySelector("date").style.backgroundColor = "black";
+}
+
+//var bac=document.querySelector("button");
+    /* Added the below line because default output of 
+       `document.body.style.background` is "" . Therefore we 
+        need to initialize it first .
+    */
+/*    document.td.style.background="white"; 
+    bac.addEventListener("click", function(){
+    if (document.td.style.background=="white"){
+        document.td.style.background="black";
+    }else if(document.td.style.background=="black"){
+        document.td.style.background="white";
+    }
+
+    });*/
+document.getElementById("cal-button").addEventListener("click", function() {
+	var x =document.getElementById("cal-input").value; 
+	document.getElementById("demo").innerHTML = x;
+});
+//[...document.getElementsByTagName("td")].forEach(td => td.addEventListener("click", s => s.target.style.color= "white"))
+[...document.getElementsByTagName("td")].forEach(td => td.addEventListener("click", e => e.target.style.background= "black"))
+//[...document.getElementsByTagName("td")].forEach(td => td.addEventListener("click", e => e.target.style.color= "white"))
+
+
+
